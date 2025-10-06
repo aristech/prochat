e9db587013f92164cf6693bdb5d6ea9405b7a39d
aristech <aris@progressnet.gr>
2025-10-06 14:05:41 +0300

    fix1

--- webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
+++ webapp/channels/src/components/widgets/icons/mattermost_logo.tsx
@@ -2,7 +2,6 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import type {CSSProperties} from 'react';
 import {useIntl} from 'react-intl';
 
 export default function MattermostLogo(props: React.HTMLAttributes<HTMLSpanElement>) {
@@ -16,22 +15,20 @@ export default function MattermostLogo(props: React.HTMLAttributes<HTMLSpanEleme
                 role='img'
                 aria-label={formatMessage({id: 'generic_icons.mattermost', defaultMessage: 'ProChat Logo'})}
             >
-                <g transform="translate(-34.843 -50)" fill="#ff620d">
+                <g
+                    transform='translate(-34.843 -50)'
+                    fill='#ff620d'
+                >
                     <polygon
-                        points="131.61 126.4 131.68 126.36 131.69 73.588 90.862 50.011 90.843 50 90.843 61.406 121.81 79.289 121.8 120.65 90.843 138.52 90.843 149.93"
-                        fill="#ff620d"
+                        points='131.61 126.4 131.68 126.36 131.69 73.588 90.862 50.011 90.843 50 90.843 61.406 121.81 79.289 121.8 120.65 90.843 138.52 90.843 149.93'
+                        fill='#ff620d'
                     />
                     <polygon
-                        points="59.883 132.06 59.878 90.692 90.843 72.81 90.843 61.403 90.824 61.414 50 84.992 50.006 137.76 50.075 137.8 90.843 161.33 90.843 149.92"
-                        fill="#ff620d"
+                        points='59.883 132.06 59.878 90.692 90.843 72.81 90.843 61.403 90.824 61.414 50 84.992 50.006 137.76 50.075 137.8 90.843 161.33 90.843 149.92'
+                        fill='#ff620d'
                     />
                 </g>
             </svg>
         </span>
     );
 }
-
-const style: CSSProperties = {
-    fillRule: 'evenodd',
-    clipRule: 'evenodd',
-};
